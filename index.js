691ef20c831f8b485dd2bef22d213b775bef1631
Luca Malandrino <malandrinoluca3@gmail.com>
2025-02-17 15:34:05 +0100

    css

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js'
-import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/11.3.1/firebase-database.js'
+import { getDatabase, ref, push, remove } from 'https://www.gstatic.com/firebasejs/11.3.1/firebase-database.js'
 
 const firebaseConfig = {
   databaseURL : 'https://fantakids-default-rtdb.europe-west1.firebasedatabase.app/'
@@ -7,8 +7,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
-const referenceInDB = ref(database, 'fam')
-
-onValue(referenceInDB, snapshot => {
-  console.log(snapshot.val())
-})
+const famRef = ref(database, 'fam')
+const countRef = ref(database, 'count')
