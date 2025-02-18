dc2f4261d8d20e32a99843e72c72c5cfad167737
Luca Malandrino <malandrinoluca3@gmail.com>
2025-02-18 21:44:24 +0100

    Firebase

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js'
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/11.3.1/firebase-database.js'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASfWL6hCF5EUikZNPJMtiqu2aNsfxd2po",
+  authDomain: "fantakids.firebaseapp.com",
+  databaseURL: "https://fantakids-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fantakids",
+  storageBucket: "fantakids.firebasestorage.app",
+  messagingSenderId: "292289812565",
+  appId: "1:292289812565:web:a6f3c266edc06a0a7adcdc"
+}; 
+const firebaseApp = initializeApp(firebaseConfig)
+const db = getDatabase(firebaseApp)
+
 const houseIcon = document.querySelector('.fa-house')
 const scaleIcon = document.querySelector('.fa-scale-balanced')
 const main = document.querySelector('main')
